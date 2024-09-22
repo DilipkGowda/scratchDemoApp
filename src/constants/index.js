9ac0243b135aa18a162dbd140703189ef556242d
DILIP <dilipkgowda30@gmail.com>
2024-09-22 12:24:26 +0530

    Changed the motions list

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,12 +22,12 @@ export const Motions = [
   {
     id: Math.floor(Math.random() * 100),
     motionName: "Move x by -50 steps",
-    action: { x: 0, y: 50, rotate: 0 },
+    action: { x: -50, y: 0, rotate: 0 },
   },
   {
     id: Math.floor(Math.random() * 100),
     motionName: "Move y by -50 steps",
-    action: { x: 0, y: 50, rotate: 0 },
+    action: { x: 0, y: -50, rotate: 0 },
   },
   {
     id: Math.floor(Math.random() * 100),
@@ -42,7 +42,7 @@ export const Motions = [
   {
     id: Math.floor(Math.random() * 100),
     motionName: "Move x by -70 y by -30",
-    action: { x: -50, y: 30, rotate: 0 },
+    action: { x: -70, y: -30, rotate: 0 },
   },
   {
     id: Math.floor(Math.random() * 100),
